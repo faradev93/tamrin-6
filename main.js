d50c36e1fe563386fe9f3cfe69848e136a62d808
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-10-28 19:20:42 +0330

    javab ta 4

--- main.js
+++ main.js
@@ -1,24 +1,77 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const text = document.getElementById("text");
+const button = document.getElementById("changeTextBtn");
 
-setupCounter(document.querySelector('#counter'))
+button.addEventListener("click", () => {
+  text.textContent = "Text Changed";
+  text.style.color = "red";
+  text.style.transition = "1s";
+});
+// tamrin-2
+
+const body = document.querySelector("body");
+const changeBdColor = document.getElementById("changeBodyColor");
+changeBdColor.addEventListener("click", () => {
+  body.style.backgroundColor = "green";
+  body.style.transition = "1s";
+});
+// test
+
+// const EL_Btn = document.createElement("button");
+// EL_Btn.innerHTML = "Blue";
+// body.appendChild(EL_Btn);
+// EL_Btn.addEventListener("mouseover", function () {
+//   EL_Btn.style.backgroundColor = "red";
+//   EL_Btn.style.transition="3s"
+// });
+
+// Tamrin-3 (show/Hide Element's)
+const btn = document.querySelector("#btn");
+const content = document.querySelector("#content");
+
+btn.addEventListener("click", () => {
+  if (content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+});
+//
+const x = document.querySelector("#neveshte");
+const y = document.querySelector("#bezan");
+
+y.addEventListener("click", () => {
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+// Tamrin-4 (Add li to ul with Click Button)
+document
+  .querySelector("#addButton")
+  .addEventListener("click", function addItem() {
+    const newItem = document.createElement("li");
+    newItem.textContent = "آیتم جدید";
+    document.getElementById("myList").appendChild(newItem);
+  });
+// rah dovom>>>>>
+//
+// function addItem() {
+//   const newItem = document.createElement("li");
+//   newItem.textContent = "آیتم جدید";
+//   document.getElementById("myList").appendChild(newItem);
+// }
+
+
+
+
+
+
+
+// Tamrin-5
